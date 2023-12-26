99dbc8d0ab463e106d08b589d62af75e45d1eca8
KatyHummes <katielihummes15@gmail.com>
2023-12-26 19:24:58 -0300

    Botão registre

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -38,13 +38,13 @@ const toggle = (event) => {
                                     @click="event => toggle(index, event)" >
                             </div> --> 
                     </div>
-                    <button type="button" @click="toggle">
+                    <!-- <button type="button" @click="toggle">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
                         </svg>
-                    </button>
+                    </button> -->
                 </div>
                 <!-- <img :src="'/storage/' + post.image_path" alt=""
                     class="h-40 w-40 hover:scale-110 transition duration-300 ease-out rounded-2xl"> -->
